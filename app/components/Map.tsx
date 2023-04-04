4b45b850edbf829c182aecc334e05cacfbadbf06
Quoc bao <baobondev@gmail.com>
2023-04-04 08:45:54 +0700

    Create Map.tsx

---
+++ app/components/Map.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import L from 'leaflet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+import makerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import makerIcon from 'leaflet/dist/images/marker-icon.png'
+import makerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: makerIcon.src,
+  iconRetinaUrl: makerIcon2x.src,
+  shadowUrl: makerShadow.src,
+})
+
+interface MapProps {
+  center?: number[]
+}
+
+const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+const Map: React.FC<MapProps> = ({ center }) => {
+  return (
+    <MapContainer
+      center={(center as L.LatLngExpression) || [51, -0.09]}
+      zoom={center ? 4 : 2}
+      scrollWheelZoom={false}
+      className=" h-[35vh] rounded-lg"
+    ></MapContainer>
+  )
+}
+
+export default Map
+
